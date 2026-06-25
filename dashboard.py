6f3cfe5069145a79d5fcd5443dbca90dd42fba49
asierraudea-dot <asierraudea@gmail.com>
2026-06-24 20:40:05 -0500

    Delete dashboard.py

--- dashboard.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def dashboard():
-    st.set_page_config(page_title="Predicción de Pertinencia", layout="wide")
-
-    st.title("Predicción de Pertinencia")
-    st.write("Aplicación en construcción.")
